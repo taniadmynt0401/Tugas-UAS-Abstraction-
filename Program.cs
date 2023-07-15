6dd38592f6587dc2d729bb7177c89756a0a0a548
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-07-15 14:10:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+using Abstraction.TicketBookingApp;
+
+namespace TicketBookingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Meminta pengguna untuk memilih jenis tiket
+            Console.WriteLine("Pilih jenis tiket:");
+            Console.WriteLine("1. Tiket Reguler");
+            Console.WriteLine("2. Tiket VIP");
+            Console.Write("Pilihan: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            // Membuat objek TicketBooking berdasarkan jenis tiket yang dipilih
+            TicketBooking ticketBooking;
+            if (choice == 1)
+            {
+                ticketBooking = new RegularTicket();
+            }
+            else if (choice == 2)
+            {
+                ticketBooking = new VipTicket();
+            }
+            else
+            {
+                Console.WriteLine("Pilihan tidak valid. Program berakhir.");
+                return;
+            }
+
+            // Meminta pengguna untuk memasukkan informasi pemesanan tiket
+            ticketBooking.GetUserInput();
+
+            // Memanggil method BookTicket untuk memesan tiket
+            ticketBooking.BookTicket();
+        }
+    }
+}
---
+++ TicketBooking.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+
+    namespace TicketBookingApp
+    {
+        abstract class TicketBooking
+        {
+            protected string name;
+            protected string vaccinationCardNumber;
+            protected string nik;
+            protected string phoneNumber;
+            protected string cardNumber;
+
+            public abstract void GetUserInput();
+
+            public void BookTicket()
+            {
+                Console.WriteLine("Tiket berhasil dipesan untuk:");
+                Console.WriteLine("Nama: " + name);
+                Console.WriteLine("Nomor Kartu Vaksin: " + vaccinationCardNumber);
+                Console.WriteLine("NIK: " + nik);
+                Console.WriteLine("Nomor Telepon: " + phoneNumber);
+                Console.WriteLine("Nomor Kartu: " + cardNumber);
+            }
+        }
+
+        class RegularTicket : TicketBooking
+        {
+            public override void GetUserInput()
+            {
+                Console.Write("Masukkan Nama: ");
+                name = Console.ReadLine();
+                Console.Write("Masukkan Nomor Kartu Vaksin: ");
+                vaccinationCardNumber = Console.ReadLine();
+                Console.Write("Masukkan NIK: ");
+                nik = Console.ReadLine();
+                Console.Write("Masukkan Nomor Telepon: ");
+                phoneNumber = Console.ReadLine();
+                Console.Write("Masukkan Nomor Kartu: ");
+                cardNumber = Console.ReadLine();
+            }
+        }
+
+        class VipTicket : TicketBooking
+        {
+            public override void GetUserInput()
+            {
+                Console.Write("Masukkan Nama: ");
+                name = Console.ReadLine();
+                Console.Write("Masukkan Nomor Kartu Vaksin: ");
+                vaccinationCardNumber = Console.ReadLine();
+                Console.Write("Masukkan NIK: ");
+                nik = Console.ReadLine();
+                Console.Write("Masukkan Nomor Telepon: ");
+                phoneNumber = Console.ReadLine();
+                Console.Write("Masukkan Nomor Kartu: ");
+                cardNumber = Console.ReadLine();
+            }
+        }
+    }
+}
